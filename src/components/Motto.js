1c730abd4cee381da518ddacb1e739f8cbb305d4
Ephraim19 <machayoephraim@gmail.com>
2022-07-22 07:39:59 +0300

    Added motto

---
+++ src/components/Motto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Motto = () => {
+  return (
+    <section className='App-motto'>
+        <h1>LET THE UNIVERSE RECOGNIZE YOU</h1>
+    </section>
+  )
+}
+
+export default Motto
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,13 @@ import logo from "../images/mtaani_logo.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="App-nav">
-        <img src={logo} alt="logo" className="App-logo" />
-        <p className="App-info" >Home</p>
-        <p className="App-info" >Blog</p>
-        <p className="App-info" >About</p>
-        <p className="App-info" >Contact</p>
-        <p className="App-info" >Pricing</p>
-      </div>
+    <nav className="App-nav">
+      <img src={logo} alt="logo" className="App-logo" />
+      <p className="App-info">Home</p>
+      <p className="App-info">Blog</p>
+      <p className="App-info">About</p>
+      <p className="App-info">Contact</p>
+      <p className="App-info">Pricing</p>
     </nav>
   );
 };
