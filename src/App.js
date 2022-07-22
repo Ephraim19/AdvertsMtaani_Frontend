6e9176f49cb346e31d33978c84f6735d36c21845
Ephraim19 <machayoephraim@gmail.com>
2022-07-22 07:59:12 +0300

    Created the about us page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Motto from './components/Motto';
+import About from './components/About';
 function App() {
   return (
     <div>
       <Navbar />
       <Motto />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='App-card'>
+        <div className='Card-info' >
+            <h3>ABOUT US</h3>
+            <p>We are a revolutionary marketing and advertising agency that focuses on
+                last mile streets.
+            </p>
+        </div>
+    </section>
+  )
+}
+
+export default About
